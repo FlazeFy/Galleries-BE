028fe131d714aa8d4f6eb0336c547e00174d67f5
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-11-10 21:20:48 +0700

    get dictionaries by type

--- galleries/routes/routes_v1.go
+++ galleries/routes/routes_v1.go
@@ -3,6 +3,8 @@ package routes
 import (
 	"net/http"
 
+	syshandlers "galleries/modules/systems/http_handlers"
+
 	"github.com/labstack/echo"
 )
 
@@ -10,12 +12,12 @@ func InitV1() *echo.Echo {
 	e := echo.New()
 
 	e.GET("api/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Welcome to Pet-Store")
+		return c.String(http.StatusOK, "Welcome to Galleries")
 	})
 
 	// =============== Public routes ===============
-
-	// =============== Private routes (Admin) ===============
+	// Dictionary
+	e.GET("api/v1/dct/:type", syshandlers.GetDictionaryByType)
 
 	return e
 }
